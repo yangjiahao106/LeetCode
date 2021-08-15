8685d784df29bb41553eb352a8444ed4c13402d1
yangjiahao106 <1060376377@qq.com>
2021-08-15 23:49:03 +0800

    fix

--- Offer/剑指 Offer 07. 重建二叉树.py
+++ Offer/剑指 Offer 07. 重建二叉树.py
@@ -14,14 +14,34 @@ class Solution:
         if not preorder:
             return None
         root = TreeNode(preorder[0])
+
         idx = inorder.index(preorder[0])
 
         root.left = self.buildTree(preorder[1:idx + 1], inorder[:idx])  # 注意 preorder[idx + 1:]
-        root.right = self.buildTree(preorder[idx:], inorder[idx + 1:])  # 注意 preorder[idx:]
+        root.right = self.buildTree(preorder[idx + 1:], inorder[idx + 1:])  # 注意 preorder[idx+1:]
         return root
 
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        def build(l, r):
-            pass
+
+        def build(root, left, right):
+            if left > right:
+                return None
+
+            node = TreeNode(preorder[root])
+            i = index[preorder[root]]
+
+            node.left = build(root + 1, left, i - 1)
+            node.right = build(root + (i - left + 1), i + 1, right)
+            return node
+
+        index = dict()
+        for i in range(len(inorder)):
+            index[inorder[i]] = i
+
+        return build(0, 0, len(preorder) - 1)
+
+
+if __name__ == '__main__':
+    Solution().buildTree([1, 2, 3], [2, 3, 1])
--- Python3/215_数组中的第K个最大元素.py
+++ Python3/215_数组中的第K个最大元素.py
@@ -66,6 +66,32 @@ class Solution:
         return quick_sort(0, len(nums) - 1)
 
 
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # 快速排序思想
+
+        def quick_sort(left, right):
+            p = random.randint(left, right)
+            nums[right], nums[p] = nums[p], nums[right]
+            m = nums[right]
+
+            i = left
+            for j in range(left, right):
+                if nums[j] > m:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i += 1
+            nums[i], nums[right] = nums[right], nums[i]
+
+            if i == k - 1:
+                return nums[i]
+
+            if i > k - 1:
+                return quick_sort(left, i - 1)
+            else:
+                return quick_sort(i + 1, right)
+
+        return quick_sort(0, len(nums) - 1)
+
 if __name__ == '__main__':
     res = Solution().findKthLargest([2, 1], 2)
     print(res)
